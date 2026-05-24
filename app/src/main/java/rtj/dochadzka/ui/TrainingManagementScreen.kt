a824ed73a80a8957308ad1d9603b2bccd61309e8
MakuchJakub <makuch5@stud.uniza.sk>
2026-05-24 14:08:09 +0200

    na obrazovku s manažmentom treningov bolo pridane dialogove okno s pre vyber času treningu

--- app/src/main/java/rtj/dochadzka/ui/TrainingManagementScreen.kt
+++ app/src/main/java/rtj/dochadzka/ui/TrainingManagementScreen.kt
@@ -1,5 +1,10 @@
 package rtj.dochadzka.ui
 
+import android.app.TimePickerDialog
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -7,8 +12,13 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.window.Dialog
+import org.intellij.lang.annotations.JdkConstants
 import rtj.dochadzka.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -18,6 +28,7 @@ fun DatePickerModal(
     onDismiss: () -> Unit
 ) {
     val datePickerState = rememberDatePickerState()
+    val timePicerState = rememberTimePickerState()
 
     DatePickerDialog(
         onDismissRequest = onDismiss,
@@ -34,8 +45,38 @@ fun DatePickerModal(
                 Text(stringResource(R.string.spat))
             }
         }
-    ) {
-        DatePicker(state = datePickerState)
+    )
+    {
+        Column {
+            DatePicker(state = datePickerState)
+
+        }
+
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimePickerModal(
+    onTimeSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val timePickerState = rememberTimePickerState()
+
+    Dialog(onDismissRequest = onDismiss) {
+        Column {
+            TimePicker(state = timePickerState)
+            Row(horizontalArrangement = Arrangement.End){
+                TextButton(onClick = onDismiss) {
+                    Text(stringResource(R.string.spat))
+                }
+                TextButton(onClick = onDismiss) {
+                    Text(stringResource(R.string.ok))
+                }
+            }
+        }
     }
 }
 
@@ -43,7 +84,13 @@ fun DatePickerModal(
 @Composable
 fun TrainingManagementScreenPreview() {
 
-    DatePickerModal({}, {})
+    TimePickerModal ({}, {})
 
 }
+
+
+
+
+
+
 
