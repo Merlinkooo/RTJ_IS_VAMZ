60445a2514bf96e1552d9bd68b9d756374377cdf
MakuchJakub <makuch5@stud.uniza.sk>
2026-05-25 15:28:07 +0200

    do obrazovky pre manažment tréningov bol doplnený vzhľad zoznamu naplánovaných tréningov

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "rtj.dochadzka"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "rtj.dochadzka"
--- app/src/main/java/rtj/dochadzka/ui/TrainingManagementScreen.kt
+++ app/src/main/java/rtj/dochadzka/ui/TrainingManagementScreen.kt
@@ -4,7 +4,12 @@ import android.app.TimePickerDialog
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,9 +22,66 @@ import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import org.intellij.lang.annotations.JdkConstants
 import rtj.dochadzka.R
+
+
+data class Training(
+    val date: String,
+    val time: String
+)
+@Composable
+fun TrainingList(trainings: List<Training>) {
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = Modifier.padding(16.dp)
+    ) {
+
+        items(trainings.count()) { training ->
+
+            Card(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text("Dátum: ${trainings.get(training).date}")
+                    Spacer(Modifier.padding(4.dp))
+                    Text("Čas: ${trainings.get(training).time}")
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TrainingListPreview() {
+
+    val trainings = listOf(
+
+        Training(
+            date = "25.05.2026",
+            time = "18:00"
+        ),
+
+        Training(
+            date = "26.05.2026",
+            time = "10:30"
+        ),
+
+        Training(
+            date = "27.05.2026",
+            time = "07:15"
+        )
+    )
+
+    TrainingList(trainings = trainings)
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,7 +90,6 @@ fun DatePickerModal(
     onDismiss: () -> Unit
 ) {
     val datePickerState = rememberDatePickerState()
-    val timePicerState = rememberTimePickerState()
 
     DatePickerDialog(
         onDismissRequest = onDismiss,
@@ -79,6 +140,13 @@ fun TimePickerModal(
         }
     }
 }
+
+@Composable
+fun TrainingManagementScreen() {
+
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
